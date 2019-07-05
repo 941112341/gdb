8f3cfa0f7a77890c509903a753e97b4be7bdf69b
jiangshihao <941112341@qq.com>
2019-07-05 09:21:31 +0800

    随机生成数组

--- array/random.go
+++ array/random.go
@@ -1,5 +1,26 @@
 package array
 
-func RandomArray(n, left, right int) []int {
+import "math/rand"
 
+func RandomArray(n, max int) []int {
+	array := make([]int, n)
+	for key := range array {
+		array[key] = rand.Intn(max)
+	}
+	return array
 }
+
+func IsSorted(array []int) bool {
+	if len(array) == 0 {
+		return true
+	}
+	x := array[0]
+	for _, value := range array {
+		if value < x {
+			return false
+		}
+	}
+	return true
+}
+
+
